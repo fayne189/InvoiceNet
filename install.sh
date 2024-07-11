91603ee5372eb687ca3fe24e1c336da08925a3d3
FRED.H <fred.huang@sjfood.com>
2024-07-11 08:11:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo apt install -y tesseract-ocr poppler-utils libxext-dev libsm-dev libxrender-dev
 
 # create virtual environment
-virtualenv env -p python3
-source env/bin/activate
+# virtualenv env -p python3
+# source env/bin/activate
 
 pip install .
